b672a9b18c8276591e2d7112f69f737d5b9123ef
Sinevia <info@sinevia.com>
2015-01-26 19:44:22 +0000

    Adding the minified file

---
+++ fastest.min.php
@@ -0,0 +1,1 @@
+<?php function app($actions = []) {$action = (isset($_REQUEST['a']) == true AND $_REQUEST['a'] !== '') ? $_REQUEST['a'] : $actions[0];if (in_array($action, $actions)) {if (function_exists($action)) {die(call_user_func($action));}}die('Page Not Found (404)');} ?>
